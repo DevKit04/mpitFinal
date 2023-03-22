130e4d0746258078619029d9292f64d99ee07273
XXVXVII <alexvas1804@gmail.com>
2023-03-23 03:39:18 +0900

    Промежуточный результат

--- DataBase/test.py
+++ DataBase/test.py
@@ -1,3 +1,5 @@
+'''
+
 import sqlite3 as sqlt 
 
 con = sqlt.connect('Users.db')
@@ -15,3 +17,15 @@ cur.execute("""
 con.commit()
 cur.close()
 con.close()
+'''
+k = 0
+
+for a in range(0,10):
+	for b in range(0,10):
+		for c in range(0,10):
+			for d in range(0,10):
+
+				if (a+b) == (c+d):
+					k+=1
+
+print(k)
