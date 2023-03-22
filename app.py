8586d5a8d777091fe424b6b3bc712f3a669a0efe
XXVXVII <alexvas1804@gmail.com>
2023-03-22 23:01:28 +0900

    Коммит 2

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__,static_folder='static')
 
-@app.route('/')
-def index():
-	return render_template('index.html')
-
+#Отрисовка страниц по запросу
 @app.route('/registration')
 def registration():
 	return render_template('registration.html')
@@ -18,6 +16,12 @@ def auth():
 @app.route('/start')
 def start():
 	return render_template('start.html')
+
+#Обработка форм
+@app.route('/reghandler', methods=['POST'])
+def regform_handler():
+	if request.method == 'POST':
+		
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- static/scripts/registration.js
+++ static/scripts/registration.js
@@ -1,10 +1,17 @@
 let submit_button = document.querySelector('.submit-button');
 
 function validateForm(){
+
+	let registration = document.querySelector('.registration');
+
 	let email = document.querySelector('.email');
 	let login = document.querySelector('.login');
 	let password = document.querySelector('.password');
 	let repassword = document.querySelector('.repassword');
+
+	registration.submit();
+
+	alert(123);
 
 }
 
